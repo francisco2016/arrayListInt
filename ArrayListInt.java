e24c55879c6235d54a24b0484ae11c1cf73b9c02
francisco2016 <otrayama@gmail.com>
2016-02-29 19:09:27 +0100

    --0078 añadido mt clear() para vaciar la colección.

--- ArrayListInt.java
+++ ArrayListInt.java
@@ -15,7 +15,7 @@ public class ArrayListInt
      */
     public ArrayListInt()
     {
-         
+
         numeros = new int[0];
     }
 
@@ -31,40 +31,43 @@ public class ArrayListInt
         }
         numeros[array2.length] = elemento;// y termina añadiendo la última posición.
     }
-    
+
     /**
      *  añade un elemento a la colección en la posición especificada por el parámetro index. Si el valor de index
      *  no es válido, entonces no hace nada. Los elementos a continuación del añadido se desplazan una posición.
      */
     public void add(int index, int elemento){
         if (index <= numeros.length && index >= 0)//1ºcomparo si el index pasado como parámetro es válido.
-    {
-       int[] local = numeros;   //2º VL para guardar el Arrays viejo.
-       numeros = new int[local.length + 1]; //3º en el Arrays viejo, nuevo Arrays con una posición más.
-       for (int cont = 0; cont < local.length; cont++)//4º un for para ir copiando los elementos en el Arrays viejo.
-       {
-       		if (cont < index)
-          {
-          		numeros[cont] = local[cont];
-          }
-          else
-          {
-           		numeros[cont+1] = local[cont];
-          }
-       }
-       numeros[index] = elemento;//5º después del for, guardo el elemento en la posición pasada como parámetro.
+        {
+            int[] local = numeros;   //2º VL para guardar el Arrays viejo.
+            numeros = new int[local.length + 1]; //3º en el Arrays viejo, nuevo Arrays con una posición más.
+            for (int cont = 0; cont < local.length; cont++)//4º un for para ir copiando los elementos en el Arrays viejo.
+            {
+                if (cont < index)
+                {
+                    numeros[cont] = local[cont];
+                }
+                else
+                {
+                    numeros[cont+1] = local[cont];
+                }
+            }
+            numeros[index] = elemento;//5º después del for, guardo el elemento en la posición pasada como parámetro.
+        }
     }
+
+    /**
+     *para vacíar la colección
+     */ 
+    public void clear(){
+        numeros = new int[0];
+    
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
+
+    
+    
+    
+    
+    
+ 
